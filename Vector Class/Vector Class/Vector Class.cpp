ab32e84aee3e113b551ed423b3639d57cdba1165
Bibushka <bianca_bucur03@yahoo.com>
2018-04-03 21:31:07 +0300

    Vector Class_2

--- Vector Class/Vector Class/Vector Class.cpp
+++ Vector Class/Vector Class/Vector Class.cpp
@@ -2,14 +2,23 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+
+using namespace std;
 
 class Vector
 {
 public:
 	Vector(int s) : yourObject{new int[s]}, size{ s } {}
+	~Vector() { delete[] yourObject; }
 	int getSize() { return size; }
-	int* getVector() { return yourObject; }
-	int getElementAtPosition(int position) { return yourObject[position]; }
+	int getElementAtPosition(int position) { return *yourObject+position; }	
+	void addValueToPosition(int position, int value) { yourObject[position] = value; }
+	void pushBack(int element) {
+		yourObject = yourObject + size*size;
+		yourObject[size] = element;
+		size++;
+	}
 private:
 	int size;
 	int* yourObject;
@@ -19,6 +28,13 @@ private:
 
 int main()
 {
+	Vector v(5);
+	for (int i = 0; i < v.getSize; i++)
+	{
+		v.addValueToPosition(i, i);
+	}
+	cout << v.getElementAtPosition(2);
+	getchar();
     return 0;
 }
 
