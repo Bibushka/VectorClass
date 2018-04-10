23180d7d1eb69fda237acd9cc513904fc3e16a03
Bibushka <bianca_bucur03@yahoo.com>
2018-04-10 22:55:49 +0300

    Vector Class_3

--- Vector Class/Vector Class/Vector Class.cpp
+++ Vector Class/Vector Class/Vector Class.cpp
@@ -9,31 +9,42 @@ using namespace std;
 class Vector
 {
 public:
-	Vector(int s) : yourObject{new int[s]}, size{ s } {}
-	~Vector() { delete[] yourObject; }
+	Vector(int s) : array{ new int[s] }, size{ s }, elementCount{ s } {}
+	~Vector() { delete[] array; }
 	int getSize() { return size; }
-	int getElementAtPosition(int position) { return *yourObject+position; }	
-	void addValueToPosition(int position, int value) { yourObject[position] = value; }
+	int getElementAtPosition(int position) { return *(array+position); }	
+	void addValueToPosition(int position, int value) { array[position] = value; }
 	void pushBack(int element) {
-		yourObject = yourObject + size*size;
-		yourObject[size] = element;
-		size++;
-	}
-private:
-	int size;
-	int* yourObject;
+		if (elementCount == size)
+		{
+			int const s = this->getSize();
+			int newArray[s*s] = *array;
+			newArray[size] = element;
+			*array = newArray;
+			++elementCount;
+		}
+		if (elementCount < size)
+		{
+			array[elementCount] = element;
+			++elementCount;
+		}
+		}
 
+private:
+	int size, elementCount;
+	int* array;
 };
 
 
 int main()
 {
 	Vector v(5);
-	for (int i = 0; i < v.getSize; i++)
+	for (int i = 0; i < v.getSize(); i++)
 	{
 		v.addValueToPosition(i, i);
 	}
-	cout << v.getElementAtPosition(2);
+	for (int i = 0; i < v.getSize(); i++)
+		cout << v.getElementAtPosition(i) << endl;
 	getchar();
     return 0;
 }
